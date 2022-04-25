99bac472b541ca9edf7d621f3128def3a0cd29fd
Obiofiong <johnobiofiong1@gmail.com>
2022-04-25 23:46:48 +0100

    Client directory added

---
+++ client/context/me.tsx
@@ -0,0 +1,27 @@
+import { Loader } from "@mantine/core";
+import { createContext, ReactNode, useContext } from "react";
+import { RefetchOptions, RefetchQueryFilters, useQuery } from "react-query";
+import { getMe } from "../pages/api";
+import { Me, QueryKeys } from "../types";
+
+const MeContext = createContext<{
+  user: Me;
+  refetch: <TPageData>(
+    options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined
+  ) => any;
+  //@ts-ignore
+}>(null);
+
+function MeContextProvider({ children }: { children: ReactNode }) {
+  const { data, isLoading, refetch } = useQuery(QueryKeys.me, getMe);
+
+  return (
+    <MeContext.Provider value={{ user: data, refetch }}>
+      {isLoading ? <Loader /> : children}
+    </MeContext.Provider>
+  );
+}
+
+const useMe = () => useContext(MeContext);
+
+export { MeContextProvider, useMe };
